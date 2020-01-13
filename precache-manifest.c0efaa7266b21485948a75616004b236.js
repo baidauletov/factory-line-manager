f98beb1c32f25600c67a33873c7cae552d5b6c38
Sultan <baidauletov.nr@gmail.com>
2020-01-13 15:37:10 +0600

    Updates

---
+++ precache-manifest.c0efaa7266b21485948a75616004b236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0b1dbcb46c88c7989711ed7df85d3a",
+    "url": "/factory-line-manager/index.html"
+  },
+  {
+    "revision": "fb7ab2ea88b5962207c1",
+    "url": "/factory-line-manager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5251ac79cea9191a84c7",
+    "url": "/factory-line-manager/static/js/2.d14eb512.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/factory-line-manager/static/js/2.d14eb512.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb7ab2ea88b5962207c1",
+    "url": "/factory-line-manager/static/js/main.82569775.chunk.js"
+  },
+  {
+    "revision": "7c90e07cd02bc0a1e711",
+    "url": "/factory-line-manager/static/js/runtime-main.399238ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/factory-line-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
